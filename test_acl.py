9380ecd4c79ef4f883ac5f9ad20733a2227c3027
Somasree Majumder <bishnupadamajumder32@gmail.com>
2025-01-01 15:56:47 +0530

    Add files via upload

---
+++ test_acl.py
@@ -0,0 +1,465 @@
+from collections import namedtuple
+from datasette.app import Datasette
+from datasette_acl import update_dynamic_groups
+import pytest
+import pytest_asyncio
+
+ManageTableTest = namedtuple(
+    "ManageTableTest",
+    (
+        "description",
+        "setup_post_data",
+        "post_data",
+        "expected_acls",
+        "should_fail_then_succeed",
+        "expected_audit_rows",
+    ),
+)
+
+
+@pytest_asyncio.fixture
+async def ds():
+    datasette = Datasette(
+        config={
+            "plugins": {
+                "datasette-acl": {
+                    "dynamic-groups": {
+                        # Users with is_staff: True are in staff group
+                        "staff": {"is_staff": True},
+                    }
+                }
+            },
+            # Root user can edit permissions
+            "permissions": {"datasette-acl": {"id": "root"}},
+        }
+    )
+    db = datasette.add_memory_database("db")
+    await db.execute_write("create table t (id primary key)")
+    await datasette.invoke_startup()
+    yield datasette
+    # Need to manually drop because in-memory databases shared across tests
+    await db.execute_write("drop table t")
+    internal_db = datasette.get_internal_database()
+    for table in await internal_db.table_names():
+        if table.startswith("acl"):
+            await internal_db.execute_write(f"drop table {table}")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ManageTableTest._fields,
+    (
+        ManageTableTest(
+            description="Group: add insert-row",
+            setup_post_data={},
+            post_data={"group_permissions_staff_insert-row": "on"},
+            expected_acls=[
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "insert-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                }
+            ],
+            should_fail_then_succeed=[
+                dict(
+                    actor={"id": "simon", "is_staff": True},
+                    action="insert-row",
+                    resource=["db", "t"],
+                ),
+            ],
+            expected_audit_rows=[
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "insert-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "added",
+                }
+            ],
+        ),
+        ManageTableTest(
+            description="Group: remove insert-row, add update-row and delete-row",
+            setup_post_data={"group_permissions_staff_insert-row": "on"},
+            post_data={
+                "group_permissions_staff_update-row": "on",
+                "group_permissions_staff_delete-row": "on",
+            },
+            expected_acls=[
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "delete-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                },
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "update-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                },
+            ],
+            should_fail_then_succeed=[
+                dict(
+                    actor={"id": "simon", "is_staff": True},
+                    action="delete-row",
+                    resource=["db", "t"],
+                ),
+                dict(
+                    actor={"id": "simon", "is_staff": True},
+                    action="update-row",
+                    resource=["db", "t"],
+                ),
+            ],
+            expected_audit_rows=[
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "insert-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "added",
+                },
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "insert-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "removed",
+                },
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "delete-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "added",
+                },
+                {
+                    "group_name": "staff",
+                    "actor_id": None,
+                    "action_name": "update-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "added",
+                },
+            ],
+        ),
+        ManageTableTest(
+            description="New user: set with insert-row and update-row",
+            setup_post_data={},
+            post_data={
+                "new_actor_id": "newbie",
+                "new_user_insert-row": "on",
+                "new_user_update-row": "on",
+            },
+            expected_acls=[
+                {
+                    "action_name": "insert-row",
+                    "actor_id": "newbie",
+                    "database_name": "db",
+                    "group_name": None,
+                    "resource_name": "t",
+                },
+                {
+                    "action_name": "update-row",
+                    "actor_id": "newbie",
+                    "database_name": "db",
+                    "group_name": None,
+                    "resource_name": "t",
+                },
+            ],
+            should_fail_then_succeed=[
+                dict(
+                    actor={"id": "newbie"},
+                    action="insert-row",
+                    resource=["db", "t"],
+                ),
+                dict(
+                    actor={"id": "newbie"},
+                    action="update-row",
+                    resource=["db", "t"],
+                ),
+            ],
+            expected_audit_rows=[
+                {
+                    "group_name": None,
+                    "actor_id": "newbie",
+                    "action_name": "insert-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "added",
+                },
+                {
+                    "group_name": None,
+                    "actor_id": "newbie",
+                    "action_name": "update-row",
+                    "database_name": "db",
+                    "resource_name": "t",
+                    "operation_by": "root",
+                    "operation": "added",
+                },
+            ],
+        ),
+    ),
+)
+async def test_manage_table_permissions(
+    ds,
+    description,
+    setup_post_data,
+    post_data,
+    expected_acls,
+    should_fail_then_succeed,
+    expected_audit_rows,
+):
+    internal_db = ds.get_internal_database()
+
+    # Staff dynamic group should have been created on startup
+    assert (
+        await internal_db.execute(
+            "select count(*) from acl_groups where name = 'staff'"
+        )
+    ).single_value() == 1
+
+    csrf_token_response = await ds.client.get(
+        "/db/t/-/acl",
+        cookies={
+            "ds_actor": ds.client.actor_cookie({"id": "root"}),
+        },
+    )
+    csrftoken = csrf_token_response.cookies["ds_csrftoken"]
+
+    if setup_post_data:
+        setup_response = await ds.client.post(
+            "/db/t/-/acl",
+            data={**setup_post_data, "csrftoken": csrftoken},
+            cookies={
+                "ds_actor": ds.client.actor_cookie({"id": "root"}),
+                "ds_csrftoken": csrftoken,
+            },
+        )
+        assert setup_response.status_code == 302
+
+    # Permission checks should fail
+    for kwargs in should_fail_then_succeed:
+        assert not await ds.permission_allowed(**kwargs), f"Should have failed: {repr}"
+
+    # Use the /db/table/-/acl page to update permissions
+    response = await ds.client.post(
+        "/db/t/-/acl",
+        data={**post_data, "csrftoken": csrftoken},
+        cookies={
+            "ds_actor": ds.client.actor_cookie({"id": "root"}),
+            "ds_csrftoken": csrftoken,
+        },
+    )
+    assert response.status_code == 302
+
+    # Check ACLs
+    acls = [
+        dict(r)
+        for r in (
+            await internal_db.execute(
+                """
+        select
+          acl_groups.name as group_name,
+          acl.actor_id,
+          acl_actions.name as action_name,
+          acl_resources.database as database_name,
+          acl_resources.resource as resource_name
+        from acl
+        left join acl_groups on acl.group_id = acl_groups.id
+        join acl_actions on acl.action_id = acl_actions.id
+        join acl_resources on acl.resource_id = acl_resources.id
+    """
+            )
+        )
+    ]
+    assert acls == expected_acls
+
+    # Permission checks should pass now
+    for kwargs in should_fail_then_succeed:
+        assert await ds.permission_allowed(
+            **kwargs
+        ), f"Should have passed: {repr(kwargs)}"
+
+    # Check audit logs
+    AUDIT_SQL = """
+        select
+          acl_groups.name as group_name,
+          acl_audit.actor_id,
+          acl_actions.name as action_name,
+          acl_resources.database as database_name,
+          acl_resources.resource as resource_name,
+          acl_audit.operation_by,
+          acl_audit.operation
+        from acl_audit
+        left join acl_groups on acl_audit.group_id = acl_groups.id
+        join acl_actions on acl_audit.action_id = acl_actions.id
+        join acl_resources on acl_audit.resource_id = acl_resources.id
+        order by acl_audit.id
+    """
+    audit_rows = [dict(r) for r in (await internal_db.execute(AUDIT_SQL))]
+    assert audit_rows == expected_audit_rows
+
+
+@pytest.mark.asyncio
+async def test_update_dynamic_groups():
+    datasette = Datasette(
+        config={
+            "plugins": {
+                "datasette-acl": {
+                    "dynamic-groups": {
+                        "staff": {"is_staff": True},
+                    }
+                }
+            }
+        }
+    )
+    await datasette.invoke_startup()
+    db = datasette.get_internal_database()
+    # Should have those tables
+    tables = await db.table_names()
+    assert {
+        "acl_resources",
+        "acl_actions",
+        "acl_groups",
+        "acl_actor_groups",
+        "acl",
+    }.issubset(tables)
+    # Group tables should start populated
+    assert (await db.execute("select count(*) from acl_groups")).single_value() == 1
+    # But no actor groups
+    assert (
+        await db.execute("select count(*) from acl_actor_groups")
+    ).single_value() == 0
+    # An actor with is_staff: True should be added to the group
+    await update_dynamic_groups(
+        datasette, {"is_staff": True, "id": "staff"}, skip_cache=True
+    )
+    assert [dict(r) for r in (await db.execute("select * from acl_groups")).rows] == [
+        {"id": 1, "name": "staff"},
+    ]
+    assert [
+        dict(r)
+        for r in (
+            await db.execute(
+                "select actor_id, (select name from acl_groups where id = group_id) as group_name from acl_actor_groups"
+            )
+        ).rows
+    ] == [
+        {"actor_id": "staff", "group_name": "staff"},
+    ]
+    # If that user changes they should drop from the group
+    await update_dynamic_groups(
+        datasette, {"is_staff": False, "id": "staff"}, skip_cache=True
+    )
+    assert [
+        dict(r)
+        for r in (
+            await db.execute(
+                "select actor_id, (select name from acl_groups where id = group_id) as group_name from acl_actor_groups"
+            )
+        ).rows
+    ] == []
+    # Groups that are not dynamic should not be modified
+    await db.execute_write("insert into acl_groups (id, name) values (2, 'static')")
+    await db.execute_write(
+        "insert into acl_actor_groups (actor_id, group_id) values ('staff', 2)"
+    )
+    await update_dynamic_groups(
+        datasette, {"is_staff": False, "id": "staff"}, skip_cache=True
+    )
+    assert [dict(r) for r in (await db.execute("select * from acl_groups")).rows] == [
+        {"id": 1, "name": "staff"},
+        {"id": 2, "name": "static"},
+    ]
+
+
+@pytest.mark.asyncio
+async def test_table_creator_permissions():
+    datasette = Datasette(
+        config={
+            "plugins": {
+                "datasette-acl": {
+                    "table-creator-permissions": [
+                        "insert-row",
+                        "delete-row",
+                    ]
+                }
+            },
+            "permissions": {"create-table": {"id": "*"}},
+        }
+    )
+    await datasette.invoke_startup()
+    db = datasette.add_memory_database("db")
+    # Create a table
+    actor_cookie = datasette.client.actor_cookie({"id": "simon"})
+    create_response = await datasette.client.post(
+        "/db/-/create",
+        json={
+            "table": "new_table",
+            "columns": [
+                {"name": "id", "type": "integer"},
+                {"name": "title", "type": "text"},
+            ],
+            "pk": "id",
+        },
+        cookies={"ds_actor": actor_cookie},
+    )
+    assert create_response.status_code == 201
+    # That table should have insert-row and delete-row ACLs
+    acls = [
+        dict(r)
+        for r in (
+            await datasette.get_internal_database().execute(
+                """
+        select
+          acl.actor_id,
+          acl_actions.name as action_name,
+          acl_resources.database as database_name,
+          acl_resources.resource as resource_name
+        from acl
+        join acl_actions on acl.action_id = acl_actions.id
+        join acl_resources on acl.resource_id = acl_resources.id
+        where acl_resources.database = 'db'
+        and acl_resources.resource = 'new_table'
+    """
+            )
+        )
+    ]
+    assert acls == [
+        {
+            "actor_id": "simon",
+            "action_name": "insert-row",
+            "database_name": "db",
+            "resource_name": "new_table",
+        },
+        {
+            "actor_id": "simon",
+            "action_name": "delete-row",
+            "database_name": "db",
+            "resource_name": "new_table",
+        },
+    ]
+    # Permission checks too
+    assert await datasette.permission_allowed(
+        actor={"id": "simon"}, action="insert-row", resource=["db", "new_table"]
+    )
+    assert await datasette.permission_allowed(
+        actor={"id": "simon"}, action="delete-row", resource=["db", "new_table"]
+    )
+    assert not await datasette.permission_allowed(
+        actor={"id": "simon"}, action="update-row", resource=["db", "new_table"]
+    )
